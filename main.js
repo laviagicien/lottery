2c051d5d08dfebeed1bd4774a124fdc0f6c405eb
Samy <samy.langlois@gmail.com>
2020-05-28 16:51:13 +0200

    sarting working with electron

---
+++ main.js
@@ -0,0 +1,32 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({ width: 1020, height: 650 });
+    // load the dist folder from Angular
+    win.loadURL(
+      url.format({
+        pathname: path.join(__dirname, "/dist/lottery/index.html"),
+        protocol: "file:",
+        slashes: true
+      })
+    );
+
+    win.setResizable(false);
+
+    // The following is optional and will open the DevTools:
+    // win.webContents.openDevTools()
+    win.on("closed", () => {
+      win = null;
+    });
+  }
+  app.on("ready", createWindow);
+  // on macOS, closing the window doesn't quit the app
+  app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+      app.quit();
+    }
+  });
